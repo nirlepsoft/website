f740b231ce7ad7a438506f540ca77ce0453cba90
chintan patel <nirlep.soft@gmail.com>
2023-09-21 12:22:54 +0530

    changes to php file

---
+++ footer.php
@@ -0,0 +1,89 @@
+<!--start footer -->
+<footer class="footer-a">
+    <div class="footer-fist-row pt40">
+        <div class="container">
+            <div class="footer-rowset">
+                <div class="col footer-head">
+                    <h5>Contact info</h5>
+                    <ul class="footer-links-list social-linkz">
+                        <li><a href="tel:+911234567890"><span><i class="fas fa-phone-square-alt"></i></span> +91 123 4567 890 </a></li>
+                        <li><a href="tel:+911234567890"><span><i class="fab fa-whatsapp-square"></i></span> +91 123 4567 890</a></li>
+                        <li><a href="mailto:career@reevan.com"><span><i class="fas fa-envelope"></i></span> career@reevan.com</a></li>
+                        <li><a href="mailto:career@reevan.com"><span><i class="fas fa-envelope"></i></span> info@reevan.com</a></li>
+                        <li><a href="skype:reevan.company"><span><i class="fab fa-skype"></i></span> reevan-skype</a></li>
+                    </ul>
+                </div>
+                <div class="col footer-head">
+                    <h5>Follow Us</h5>
+                    <ul class="footer-links-list social-linkz">
+                        <li><a href="javascript:void(0)"><span><i class="fab fa-facebook-f"></i></span> Facebook</a>	</li>
+                        <li><a href="javascript:void(0)"><span><i class="fab fa-twitter"></i></span> Twitter </a>	</li>
+                        <li><a href="javascript:void(0)"><span><i class="fab fa-instagram"></i></span> Instagram</a> </li>
+                        <li><a href="javascript:void(0)"><span><i class="fab fa-pinterest-p"></i></span> Pinterest</a></li>
+                        <li><a href="javascript:void(0)"><span><i class="fab fa-linkedin-in"></i></span> linkedin</a></li>
+                        <li><a href="javascript:void(0)"><span><i class="fab fa-youtube"></i></span> Youtube</a> </li>
+                    </ul>
+                </div>
+                <div class="col footer-head">
+                    <h5>Services</h5>
+                    <ul class="footer-links-list">
+                        <li><a href="javascript:void(0)">Hire Dedicated Developers</a></li>
+                        <li><a href="javascript:void(0)">Web App Development</a></li>
+                        <li><a href="javascript:void(0)">Mobile App Development</a></li>
+                        <li><a href="javascript:void(0)">Cloud Services</a></li>
+                        <li><a href="javascript:void(0)">Product Design</a></li>
+                    </ul>
+                </div>
+<!--                <div class="col footer-head">-->
+<!--                    <h5>Industries</h5>-->
+<!--                    <ul class="footer-links-list">-->
+<!--                        <li><a href="javascript:void(0)">Healthcare</a></li>-->
+<!--                        <li><a href="javascript:void(0)">Education</a></li>-->
+<!--                        <li><a href="javascript:void(0)">Retail</a></li>-->
+<!--                        <li><a href="javascript:void(0)">Logistics</a></li>-->
+<!--                        <li><a href="javascript:void(0)">Oil & Gas</a></li>-->
+<!--                        <li><a href="javascript:void(0)">Music & Video</a></li>-->
+<!--                    </ul>-->
+<!--                </div>-->
+<!--                <div class="col footer-head">-->
+<!--                    <h5>Portfolio</h5>-->
+<!--                    <ul class="footer-links-list">-->
+<!--                        <li><a href="javascript:void(0)">StockNow - Investment App</a></li>-->
+<!--                        <li><a href="javascript:void(0)">Dochelp - Patient Monitoring</a></li>-->
+<!--                        <li><a href="javascript:void(0)">Roster - Pizza Delivery</a></li>-->
+<!--                        <li><a href="javascript:void(0)">Nikea - Logo Design</a></li>-->
+<!--                        <li><a href="javascript:void(0)">Eptire - Blockchain Solution</a></li>-->
+<!--                        <li><a href="javascript:void(0)">ShopTop - Grocery App</a></li>-->
+<!--                    </ul>-->
+<!--                </div>-->
+            </div>
+        </div>
+        <div class="footer-abt">
+
+        </div>
+        <div class="container ft-cpy">
+            <div class="row">
+                <div class="col-lg-5">
+                    <div class="ft-copyright">
+                        <p>We are tracking any intention of piracy.</p>
+                    </div>
+                </div>
+                <div class="col-lg-7">
+                    <div class="ft-copyright ft-r">
+                        <p>© <?php echo date('Y');?> All Rights Reserved By </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- end footer -->
+<!--All js files-->
+<script src="js/modernizr-3.5.0.min.js"></script>
+<script src="js/jquery-3.6.0.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/app.bundle.js"></script>
+<!--Common reevan script file-->
+<script src="js/main.js"></script>
+</body>
+</html>
